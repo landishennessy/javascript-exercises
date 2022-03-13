ff85c7e30200efabb6606292cf1ceb8a631d05cb
lando <91922837+landishennessy@users.noreply.github.com>
2022-03-13 12:47:41 -0600

    Think its working
    I cant figure out how to install jest on windows to use the tests so i am going to do it on my linux machine

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,34 @@
-const add = function() {
+const add = function(a , b) {
+  return (a + b);
 	
 };
 
-const subtract = function() {
+const subtract = function(a , b) {
+  return (a - b);
 	
 };
 
-const sum = function() {
+const sum = function(a , b) {
+  return ( a + b);
 	
 };
 
-const multiply = function() {
+const multiply = function(a , b) {
+  return(a*b);
 
 };
 
-const power = function() {
+const power = function(a , b) {
+  return Math.pow(a,b);
 	
 };
 
-const factorial = function() {
+const factorial = function(n) {
+  if(n === 0 || n===1){
+    return 1;
+  } else{
+    return n * factorial(n-1);
+  }
 	
 };
 
